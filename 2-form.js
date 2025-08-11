0667ac08d3337e0ca79d195613651240f8484fa4
Reck1essness <crossdmitry@gmail.com>
2025-08-11 15:11:19 +0000

    Deploying to gh-pages from @ Reck1essness/goit-js-hw-09@c7b539ed9a9ad3e7634fb768e7ea7374590b1213 🚀

---
+++ 2-form.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";const t=document.getElementById("message");t.addEventListener("input",function(){this.style.height="auto",this.style.height=this.scrollHeight+"px"});
+//# sourceMappingURL=2-form.js.map
